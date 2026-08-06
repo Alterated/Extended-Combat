35d0f4044961155ed512532a6883af210a554d51
PDP1 <alxwooton@gmail.com>
2026-08-06 11:35:16 -0500

    working on adding recipes to the naginatas

---
+++ src/main/java/mods/PDP1/extended_combat/block/ModBlocks.java
@@ -0,0 +1,12 @@
+package mods.PDP1.extended_combat.block;
+
+import mods.PDP1.extended_combat.Extendedcombat;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, Extendedcombat.MODID);
+}
---
+++ src/main/java/mods/PDP1/extended_combat/datagen/ModBlockTagGenerator.java
@@ -0,0 +1,21 @@
+package mods.PDP1.extended_combat.datagen;
+
+import mods.PDP1.extended_combat.Extendedcombat;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagGenerator extends BlockTagsProvider {
+    public ModBlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, Extendedcombat.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+
+    }
+}
